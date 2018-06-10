First complete part of image movement recognition with some errors, need to fix the normalization of the columns

Movimientos/recurrence.py
@@ -1,5 +1,7 @@
-# 05 de Junio del 2018
+# 05 de Junio del 2018 <- Se implementa en modulo interno
+# Funcion para salvar imagenes ( Tutorial conv )
 
+# 01 de Junio del 2018
 # 31 Mayo 2018
 
 import numpy as np
@@ -26,7 +28,7 @@ class Recurrent_Photo:
         self.camera.release()
         self.resize = resize
 
-    def get_recurrence(self, alpha=(0.75555, 0.25555)):
+    def get_recurrence(self, alpha=(0.55, 0.45)):
         '''
         Alpha are 2 float numbers represented by the amount of 
         superposition that you want to have in te current image.
@@ -53,12 +55,14 @@ class Recurrent_Photo:
         '''
         return np.abs(A - B)
 
-def resize_images(X, dimensions=(100, 75)):
+def resize_images(X, dimensions=(100, 100)):
     if len(X.shape) == 3:
         X = cv2.resize(X, dimensions)
-    else:    
-        for image in X:
-            image = cv2.resize(image, dimensions)
+    else:
+        resized_images = np.zeros([len(X), dimensions[0], dimensions[1], 3])
+        for image in range(len(X)):
+            resized_images[image] = cv2.resize(X[image], dimensions)
+        X = resized_images
     return X
 
 def show_image(X):
@@ -69,20 +73,4 @@ def show_image(X):
             cv2.imshow('X', image)
             cv2.waitKey(1)
             sleep(0.05)
-
-non_movement = Recurrent_Photo(50)
-
-print('Prepare next movement...')
-sleep(2)
-
-movement = Recurrent_Photo(50)
-
-non_movement_recurrence = non_movement.get_recurrence()
-movement_recurrence = movement.get_recurrence()
-
-X = resize_images(non_movement_recurrence)
-Y = resize_images(movement_recurrence)
-
-
-
 

TestMovimientos/unitary_pixel_test.py
@@ -7,6 +7,8 @@ camera = cv2.VideoCapture(0)
 previous_photos = 0
 X = []
 N = []
+m = []
+dx = []
 for vc in range(100):
     _, photo = camera.read()
     cv2.imshow('Image', photo)
@@ -17,14 +19,23 @@ for vc in range(100):
     # Add X to plot
     X.append(point)
 
+    # Derivative
+    if vc > 0:
+        dx.append(X[-1]-X[-2])
     # Normalization
-    N.append(np.sum(X)/len(X))
-    m = 10
+    # norm = np.sum(X)/len(X)
+    # N.append(norm)
+    # if vc > 0:
+    #     m.append(np.abs(norm - N[-2]))
     
     # Printing Data
     print(photo[0, 0, 0])
     cv2.waitKey(1)
-plt.plot(range(100), comportament)
+plt.plot(range(100), X)
 plt.show()
-plt.plot(range(100), normalization)
+plt.plot(range(99), dx)
 plt.show()
+# plt.plot(range(100), N)
+# plt.show()
+# plt.plot(range(99), m)
+# plt.show()