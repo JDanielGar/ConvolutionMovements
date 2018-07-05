be0a37a805a95cd6ed53b5b630d738dbf78bc1c5
Daniel Garcia <jdaniel_gar@hotmail.com>
2018-07-05 16:51:22 -0500

    Movement with weights

---
+++ Movimientos/convolve_binary.py
@@ -0,0 +1,80 @@
+# Junio 5 del 2018
+# Observar las siguientes paginas:
+# 1. https://cambridgespark.com/content/tutorials/convolutional-neural-networks-with-keras/index.html
+# 2. https://www.youtube.com/watch?v=FmpDIaiMIeA
+# (Las dos se complementan, observar cambios)
+# (Opcional) 3. https://cambridgespark.com/content/tutorials/deep-learning-for-complete-beginners-recognising-handwritten-digits/index.html 
+
+
+from recurrence import Recurrent_Photo, resize_images, show_image, real_time_classification
+
+from keras import backend
+from keras.models import Sequential
+from keras.layers import Input, Conv2D, MaxPooling2D, Dense, Dropout, Flatten
+
+from sklearn.utils import shuffle
+
+from time import sleep
+
+import numpy as np
+
+''' Take the data from enviroment '''
+
+left = Recurrent_Photo(20, times=3).recurrence_image
+right = Recurrent_Photo(20, times=3).recurrence_image
+
+''' Prepare the data to train '''
+
+A = resize_images(left, (100, 100))
+A_labels = np.zeros(A.shape[0])
+
+B = resize_images(right, (100, 100))
+B_labels = np.ones(B.shape[0])
+
+X_train = np.concatenate((A, B), axis=0)
+Y_train = np.concatenate((A_labels, B_labels), axis=0)
+X_train, Y_train = shuffle(X_train, Y_train)
+
+
+''' Prepare layers of network '''
+
+def get_classifier():
+    classifier = Sequential()
+    classifier.add(Conv2D(32, (3, 3), input_shape=(100, 100, 3), activation = 'relu'))
+    classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+    classifier.add(MaxPooling2D(pool_size = (2, 2)))
+    classifier.add(Dropout(0.25))
+    classifier.add(Conv2D(58, (3, 3), activation = 'relu'))
+    classifier.add(Conv2D(58, (3, 3), activation = 'relu'))
+    classifier.add(MaxPooling2D(pool_size = (2, 2)))
+    classifier.add(Dropout(0.25))
+    classifier.add(Flatten())
+    classifier.add(Dense(output_dim = 512, activation = 'relu'))
+    classifier.add(Dropout(0.5))
+    classifier.add(Dense(output_dim = 1, activation = 'sigmoid'))
+    classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return classifier
+
+
+''' Compiling classificator '''
+    
+classifier = get_classifier()
+classifier.load_weights('./movement_weights')
+
+for images in X_train:
+   print(classifier.predict(np.array([images])))
+
+new_classifier = get_classifier()
+
+batch_size = 32 
+num_epochs = 10 
+
+classifier.compile(loss='binary_crossentropy', 
+              optimizer='adam',
+              metrics=['accuracy'])
+
+classifier.fit(X_train, Y_train,              
+          batch_size=batch_size, epochs=num_epochs,
+          verbose=1, validation_split=0.1)
+
+real_time_classification(10, classifier)
--- Movimientos/recurrence.py
+++ Movimientos/recurrence.py
@@ -7,16 +7,11 @@ from time import sleep
 
 
 class Recurrent_Photo():
-    def __init__(self, capacity, humbral=255/2):
+    def __init__(self, capacity, humbral=255/2, times=1):
         capture = cv2.VideoCapture(0)
 
         self.last_sequence = []
-        self.recurrence_image = np.zeros([capacity, 720, 1280, 3])
-
-        # Counter
-        for counter in range(3):
-            print(counter+1)
-            sleep(1)
+        self.recurrence_image = np.zeros([capacity*times, 720, 1280, 3])
 
         image = capture.read()[1]/255
         
@@ -24,30 +19,81 @@ class Recurrent_Photo():
 
         alfa_sequence = image 
 
-        for frame in range(capacity):
-            # Capture image
-            image = capture.read()[1]/255
+        for time in range(times):
+            for counter in range(3):
+                print(counter+1)
+                sleep(1)
+            for frame in range(capacity):
+                # Capture image
+                image = capture.read()[1]/255
 
-            # Push image into array
-            self.last_sequence.append(image)
+                # Push image into array
+                self.last_sequence.append(image)
 
-            # Get diference
-            image = np.abs(image - self.last_sequence[-2])
+                # Get diference
+                image = np.abs(image - self.last_sequence[-2])
 
-            # Get 0 when not change is detected
-            image[image[:, :] >= humbral] = 0 
+                # Get 0 when not change is detected
+                image[image[:, :] >= humbral] = 0 
 
-            # Alfa sequence
-            alfa_sequence = image + alfa_sequence/1.5
+                # Alfa sequence
+                alfa_sequence = image + alfa_sequence/1.5
 
-            # Result into a numpy array
-            self.recurrence_image[frame] = alfa_sequence
+                # Result into a numpy array
+                self.recurrence_image[frame] = alfa_sequence
 
-            # Normal CV2 Process
-            cv2.imshow('Image', alfa_sequence)
-            cv2.waitKey(1)
+                # Normal CV2 Process
+                cv2.imshow('Image', alfa_sequence)
+                cv2.waitKey(1)
     
         capture.release()
+
+def real_time_classification(times, classifier, humbral=255/2):
+    '''
+    Only works with keras classifier object.
+    '''
+    capture = cv2.VideoCapture(0)
+
+    last_sequence = [0, 0]
+
+    image = capture.read()[1]/255
+
+    last_sequence[0] = image
+
+    alfa_sequence = image 
+
+    for times in range(times):
+        # Capture image
+        image = capture.read()[1]/255
+        
+        # Push image into array
+        last_sequence.append(image)
+
+        # Get diference
+        image = np.abs(image - last_sequence[-2])
+
+        # Get 0 when not change is detected
+        image[image[:, :] >= humbral] = 0 
+
+        # Alfa sequence
+        alfa_sequence = image + alfa_sequence/1.5
+
+        prediction = classifier.predict(np.array([resize_images(alfa_sequence)]))
+
+        # Normal CV2 Process
+        image_with_text = np.array(alfa_sequence)
+        cv2.putText(image_with_text, 
+            str(prediction),
+            (100, 500),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (255, 255, 255),
+            2
+        )
+        cv2.imshow('Image', image_with_text)
+        cv2.waitKey(1)
+
+        last_sequence.pop(-2)
 
 def resize_images(X, dimensions=(100, 100)):
     if len(X.shape) == 3:
@@ -67,3 +113,4 @@ def show_image(X):
             cv2.imshow('X', image)
             cv2.waitKey(1)
             sleep(0.05)
+
